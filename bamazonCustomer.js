8201bcfdac079eab3e8200b26015084dc15ea03f
BlaiseAK <blaise.a.kress@gmail.com>
2018-07-06 10:04:35 -0400

    added some inquirer prompts and validated inputs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,6 +17,8 @@ connection.connect(function(err) {
     if(err) throw err;
     console.log("connected as id " + connection.threadId);
     // fire off inquirer function
+    customerOptions();
+
 });
 
 function customerOptions() {
@@ -24,8 +26,51 @@ function customerOptions() {
         {
             name: "options",
             message: "What would you like to do today?",
-            input: "list",
+            type: "list",
             choices: ["Buy an item", "Sell an item"]
+        }
+    ).then(function(res){
+        var choice = res.options;
+        if (choice === 'Buy an item') {
+            // Show products to be sold
+            showItemsToBuy();
+        }
+    }).catch(function(err) {
+        console.log(err);
+    })
+}
+
+function showItemsToBuy() {
+    connection.query("SELECT id, product_name, price FROM products", function(err,res) {
+        if (err) throw err;
+
+        console.log(res);
+        selectItemIdToBuy();
+        connection.end();
+    })
+}
+
+function selectItemIdToBuy() {
+    inquirer.prompt(
+        {
+            name: "idToBuy",
+            message: "What is the items id that you wish to buy?",
+            validate: function(value) {
+                if (isNaN(value) === false && parseInt(value) > 0 && parseInt(value) <= 100) {
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            name: "qtyToBuy",
+            message: "How many would you like to buy?",
+            validate: function(value) {
+                if (isNaN(value) === false && parseInt(value) > 0 && parseInt(value) <= 100) {
+                    return true;
+                }
+                return false;
+            }
         }
     )
 }
